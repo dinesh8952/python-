8889120eac67dc1078e0076c450708434cda1137
DINESH <dinesh@DINESHs-MacBook-Air.local>
2025-02-24 00:46:11 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Adding 2 + 3:", add(2, 3))
+    print("Subtracting 5 - 3:", subtract(5, 3))
+
