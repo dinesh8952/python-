4d7dda6e52bfe57b838fa0a4ae04551cd5e491b9
dinesh8952 <129853237+dinesh8952@users.noreply.github.com>
2025-02-24 01:33:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 def add(a, b):
     return a + b
 
+
 def subtract(a, b):
     return a - b
+
 
 if __name__ == "__main__":
     print("Adding 2 + 3:", add(2, 3))
     print("Subtracting 5 - 3:", subtract(5, 3))
-
